48c2a1d8be5c7f4f0e475bd15794b4dced642605
ror_inx <pratikraj@ROR06.(none)>
2013-01-19 15:58:29 +0530

    master password encryption and decryption

--- app/models/master_password.rb
+++ app/models/master_password.rb
@@ -1,3 +1,22 @@
 class MasterPassword < ActiveRecord::Base
 	validates :mpassword, :presence => true
+  before_save :encryption
+
+
+
+  def encryption
+    key =  ActiveSupport::MessageEncryptor.new(algo)
+    return  self.mpassword = key.encrypt(self.mpassword)
+  end
+
+  def self.decrypt
+    key =  ActiveSupport::MessageEncryptor.new(Digest::SHA1.hexdigest("fabtab"))
+    return self.first.mpassword = key.decrypt(self.first.mpassword)
+  end
+
+  private
+
+  def algo
+    return  secret = Digest::SHA1.hexdigest("fabtab")
+  end
 end
