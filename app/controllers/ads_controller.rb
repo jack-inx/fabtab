721e99622e7a95aff2f9ccfb2636662b00eefbe9
ror-inx <ror-inx@ror07.(none)>
2013-02-20 19:01:52 +0530

    ads controller

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -58,7 +58,7 @@ class AdsController < ApplicationController
     @ad = Ad.new(params[:ad])
     if !params[:ad][:image_url].nil?
       @ad_image = open(params[:ad][:image_url])
-      @ad_image.image_file_name = "#{current_user.id + (Time.now).to_i}.jpg"
+      @ad_image.name = "#{current_user.id + (Time.now).to_i}.jpg"
       @ad.image = @ad_image
     end
     @ad.ad_type = "url"
